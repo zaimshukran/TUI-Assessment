5ef1d714d5b5c8ab78980041bb0f71698b393056
zaimshukran <zaimshukran21@gmail.com>
2025-08-11 04:41:50 +0800

    Make the ec2 public

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,27 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
 resource "aws_subnet" "main_subnet" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
@@ -14,6 +35,11 @@ resource "aws_subnet" "main_subnet" {
   tags = {
     Name = "main-subnet"
   }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_security_group" "allow_ssh" {
@@ -41,11 +67,12 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 resource "aws_instance" "ubuntu" {
-  ami                    = "ami-02c7683e4ca3ebf58"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.main_subnet.id
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-  key_name               = var.key_name
+  ami                         = "ami-02c7683e4ca3ebf58"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.main_subnet.id
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  key_name                   = var.key_name
+  associate_public_ip_address = true
 
   tags = {
     Name = "ubuntu-instance"
