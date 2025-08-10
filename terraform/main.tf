bc3b0a211f59a4b77fe480bc919cfe5024b1dcc9
zaimshukran <zaimshukran21@gmail.com>
2025-08-11 04:14:18 +0800

    Adding availability zone correction

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main_subnet" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-southeast-1"
+  availability_zone = "ap-southeast-1-a"
 
   tags = {
     Name = "main-subnet"
